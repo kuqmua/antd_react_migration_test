7568e472fe7cd8fa0464bc4f2b52d95bd4b26d3e
kuqmua <serezhashmakov@outlook.com>
2022-07-05 22:24:41 +0300

    backtop

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Badge, Alert } from 'antd';
+import { Badge, Alert, BackTop } from 'antd';
 // const { Badge } = antd;
 import Button from 'antd/es/button';
 import 'antd/dist/antd.css';
@@ -9,13 +9,15 @@ import 'antd/dist/antd.css';
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Primary</Button>
+      {/* <Button type="primary">Primary</Button> */}
       {/* <BackTop/> */}
       {/* <Badge count={43}>
         <div style={{"backgroundColor": "red", width: "300px", "height": "300px"}}>fgfgh</div>
       </Badge> */}
-      <Badge count={5}/>
-      <Alert message="fddfg"/>
+      {/* <Badge count={5}/>
+      <Alert message="fddfg"/> */}
+      <BackTop/>
+      <div style={{"backgroundColor": "red", width: "300px", "height": "1500px"}}>fgfgh</div>
       {/* <a href="#" className="head-example" /> */}
     {/* </Badge> */}
     </div>
