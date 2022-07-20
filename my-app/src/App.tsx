7103d46aaa246f74f6fce3a3423b418ea74b83ae
kuqmua <serezhashmakov@outlook.com>
2022-07-20 19:13:46 +0300

    excplicit progress

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,16 @@
 // import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Badge, Alert, BackTop } from 'antd';
+// import { Badge, Alert, BackTop } from 'antd';
 // const { Badge } = antd;
-import Button from 'antd/es/button';
-import 'antd/dist/antd.css';
+// import Button from 'antd/es/button';
+// import 'antd/dist/antd.css';
+// import { Line, Circle } from 'rc-progress';
+import { Line, Circle } from './progress';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ "backgroundColor": "black", width: "100%", "height": "100%" }}>
       {/* <Button type="primary">Primary</Button> */}
       {/* <BackTop/> */}
       {/* <Badge count={43}>
@@ -16,10 +18,12 @@ function App() {
       </Badge> */}
       {/* <Badge count={5}/>
       <Alert message="fddfg"/> */}
-      <BackTop/>
-      <div style={{"backgroundColor": "red", width: "300px", "height": "1500px"}}>fgfgh</div>
+      {/* <BackTop /> */}
+      <Line percent={10} strokeWidth={4} strokeColor="#D3D3D3" trailColor="#D9D9D9" />
+      <Circle percent={25} strokeWidth={4} strokeColor="#D3D3D3" trailColor="#D9D9D9" />
+      {/* <div style={{"backgroundColor": "red", width: "300px", "height": "1500px"}}>fgfgh</div> */}
       {/* <a href="#" className="head-example" /> */}
-    {/* </Badge> */}
+      {/* </Badge> */}
     </div>
   );
 }
---
+++ my-app/src/progress/common.ts
@@ -0,0 +1,45 @@
+import { useRef, useEffect } from 'react';
+import type { ProgressProps } from './interface';
+
+export const defaultProps: Partial<ProgressProps> = {
+    className: '',
+    percent: 0,
+    prefixCls: 'rc-progress',
+    strokeColor: '#2db7f5',
+    strokeLinecap: 'round',
+    strokeWidth: 1,
+    style: {},
+    trailColor: '#D9D9D9',
+    trailWidth: 1,
+    gapPosition: 'bottom',
+};
+
+export const useTransitionDuration = (): SVGPathElement[] => {
+    const pathsRef = useRef<SVGPathElement[]>([]);
+    const prevTimeStamp = useRef(null);
+
+    useEffect(() => {
+        const now = Date.now();
+        let updated = false;
+
+        pathsRef.current.forEach((path) => {
+            if (!path) {
+                return;
+            }
+
+            updated = true;
+            const pathStyle = path.style;
+            pathStyle.transitionDuration = '.3s, .3s, .3s, .06s';
+
+            if (prevTimeStamp.current && now - prevTimeStamp.current < 100) {
+                pathStyle.transitionDuration = '0s, 0s';
+            }
+        });
+
+        if (updated) {
+            (prevTimeStamp.current as any) = Date.now();
+        }
+    });
+
+    return pathsRef.current;
+};
